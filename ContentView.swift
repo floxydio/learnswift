3ede3990b33280ce5fb5be29f5f74955811d2436
floxydio <diooktar@gmail.com>
2022-12-07 23:39:39 +0700

    update view api

--- ContentView.swift
+++ ContentView.swift
@@ -57,6 +57,33 @@ struct ContentView: View {
 }
 
 
+struct TabBarViewHome: View {
+    var body: some View {
+        ZStack{
+            TabView{
+                HomeView().tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                AboutView().tabItem {
+                                   Image(systemName: "paperplane.fill")
+                                   Text("Explore")
+                               }
+                HomeView().tabItem {
+                                   Image(systemName: "tray.fill")
+                                   Text("Subscription")
+                               }
+                HomeView().tabItem {
+                                   Image(systemName: "envelope.fill")
+                                   Text("Kotak Masuk")
+                               }
+            }
+        }.accentColor(Color.red)
+    }
+    
+}
+
+
 struct HomeView: View {
     var body: some View {
         NavigationView{
@@ -133,9 +160,41 @@ struct ContentList: View {
 }
 
 
+struct FetchApiPreview: View {
+        @State var results = [TaskEntry]()
+    func loadData() {
+        guard let url = URL(string:"https://jsonplaceholder.typicode.com/todos") else {
+            print("Error")
+            return
+        }
+        let request =  URLRequest(url: url)
+        
+        
+        URLSession.shared.dataTask(with: request) {
+            data,response, error in if let data = data {
+                if let response = try? JSONDecoder().decode([TaskEntry].self, from: data) {
+                    DispatchQueue.main.async {
+                            self.results = response
+                        
+                    }
+                    return
+                }
+            }
+        }.resume()
+    }
+    var body: some View {
+        List(results, id: \.id) {
+            item in VStack(alignment: .leading){
+                Text(item.title)
+            }
+        }.onAppear(perform: loadData)
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        TabBarViewHome()
     }
 }
 
---
+++ Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  SwiftLearn
+//
+//  Created by Dio Rovelino on 07/12/22.
+//  Copyright © 2022 Dio Rovelino. All rights reserved.
+//
+
+import Foundation
+
+struct TaskEntry: Codable  {
+    let id: Int
+    let title: String
+}
+
+
+
